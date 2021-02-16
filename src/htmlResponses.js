5254eea482da75f4f90425d4f6447f04f43c30d8
FrankLuo426 <33915033+FrankLuo426@users.noreply.github.com>
2021-02-17 03:04:15 +0800

    push

---
+++ src/htmlResponses.js
@@ -0,0 +1,21 @@
+const errorPage = `
+<html>
+    <head>
+        <title>404 - File Not Found!</title>
+    </head>
+    <body>
+        <h1>404 - File Not Found!</h1>
+        <p>Check your URL, or your typing!</p>
+        <p>:-o</p>
+    </body>
+</html>`;
+
+const get404Response = (request, response) => {
+  response.writeHead(404, {
+    'Content-Type': 'text/html',
+  });
+  response.write(errorPage);
+  response.end();
+};
+
+module.exports.get404Response = get404Response;
--- src/index.js
+++ src/index.js
@@ -1,116 +1,34 @@
-// 1 - pull in the HTTP server module
 const http = require('http');
-
-// 2 - pull in URL and query modules (for URL parsing)
 const url = require('url');
-//const query = require('querystring');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
-// 3 - locally this will be 3000, on Heroku it will be assigned
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// 5 - here's our 404 page
-const errorPage = `
-<html>
-    <head>
-        <title>404 - File Not Found!</title>
-    </head>
-    <body>
-        <h1>404 - File Not Found!</h1>
-        <p>Check your URL, or your typing!</p>
-        <p>:-o</p>
-    </body>
-</html>`;
-
-// 6 - this will return a random number no bigger than `max`, as a string
-// we will also doing our query parameter validation here
-// const getRandomNumberJSON = (max = 1) => {
-//     max = Number(max);
-//     max = !max ? 1 : max;
-//     max = max < 1? 1 : max;
-//     const number = Math.random() * max;
-//     const responseObj = {
-//         timestamp: new Date(),
-//         number: number
-//     };
-//     return JSON.stringify(responseObj);
-// };
-
-const getRandomJokeJSON = () => {
-  const jokes = [{
-    q: 'What do you call a very small valentine?',
-    a: 'A valen-tiny!',
-  },
-  {
-    q: 'What did the dog say when he rubbed his tail on the sandpaper?',
-    a: 'Ruff, Ruff!',
-  },
-  {
-    q: "Why don't sharks like to eat clowns?",
-    a: 'Because they taste funny!',
-  },
-  {
-    q: 'What did the boy cat say to the girl cat?',
-    a: "You're Purr-fect!",
-  },
-  {
-    q: "What is a frog's favorite outdoor sport?",
-    a: 'Fly Fishing!',
-  },
-  {
-    q: 'I hate jokes about German sausages.',
-    a: 'Theyre the wurst.',
-  },
-  {
-    q: 'Did you hear about the cheese factory that exploded in France?',
-    a: 'There was nothing left but de Brie.',
-  },
-  {
-    q: 'Our wedding was so beautiful ',
-    a: 'Our wedding was so beautiful ',
-  },
-  {
-    q: 'Is this pool safe for diving?',
-    a: 'It deep ends.',
-  },
-  {
-    q: 'Dad, can you put my shoes on?',
-    a: 'I dont think theyll fit me.',
-  },
-  ];
-  const responseObj = jokes[Math.floor(Math.random() * 10)];
-  return JSON.stringify(responseObj);
+const urlStruct = {
+  '/random-joke': jsonHandler.getRandomJokeResponse,
+  notFound: htmlHandler.get404Response,
+  '/random-jokes': jsonHandler.getRandomJokeResponse,
 };
 
-// 7 - this is the function that will be called every time a client request comes in
-// this time we will look at the `pathname`, and send back the appropriate page
-// note that in this course we'll be using arrow functions 100% of the time in our server-side code
 const onRequest = (request, response) => {
-  // console.log(request.headers);
   const parsedUrl = url.parse(request.url);
-  const { pathname } = parsedUrl;
-  console.log('parsedUrl=', parsedUrl);
-  console.log('pathname=', pathname);
+  const {
+    pathname,
+  } = parsedUrl;
 
-  // const params = query.parse(parsedUrl.query);
-  // const max = params.max;
-  // console.log("params=", params);
-  // console.log("max=", max);
+  const params = query.parse(parsedUrl.query);
 
-  if (pathname === '/') {
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
-    response.write(getRandomJokeJSON());
-    response.end();
+  // let acceptedTypes = request.headers.accept && request.headers.accept.split(',');
+  // acceptedTypes = acceptedTypes || [];
+
+  if (urlStruct[pathname]) {
+    urlStruct[pathname](request, response, params);
   } else {
-    response.writeHead(404, {
-      'Content-Type': 'text/html',
-    });
-    response.write(errorPage);
-    response.end();
+    urlStruct.notFound(request, response);
   }
 };
 
-// 8 - create the server, hook up the request handling function, and start listening on `port`
 http.createServer(onRequest).listen(port);
 console.log(`Listening on 127.0.0.1: ${port}`);
