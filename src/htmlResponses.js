d3bc0d4b60976c29a49862735bf2ef07eea2783b
FrankLuo426 <33915033+FrankLuo426@users.noreply.github.com>
2021-02-18 23:11:08 +0800

    a

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -6,6 +6,7 @@ const errorPage = `
     <body>
         <h1>404 - File Not Found!</h1>
         <p>Check your URL, or your typing!</p>
+        <p>Pherhps you are looking for <a href="/random-joke">/random-joke</a> or <a href="/random-jokes?limit=10">/random-jokes?limit=10</a>?
         <p>:-o</p>
     </body>
 </html>`;
